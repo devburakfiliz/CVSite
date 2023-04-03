bc917e3aed6c1c7a8a0ba65b446d9720aa87d935
Burak Filiz <dev.burakfiliz@gmail.com>
2023-04-03 14:00:08 +0300

    Added Skill page

---
+++ WebUI/Dtos/SkillDto/CreateSkillDeto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Dtos.SkillDto
+{
+    public class CreateSkillDeto
+    {
+        public string SkillName { get; set; }
+        public string SkillImage { get; set; }
+    }
+}
--- WebUI/WievComponents/Default/_SkillPartial.cs
+++ WebUI/WievComponents/Default/_SkillPartial.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using WebUI.Dtos.SkillDto;
 
 namespace WebUI.WievComponents.Default
 {
     public class _SkillPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SkillPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:3509/api/Skill");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSkillDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
